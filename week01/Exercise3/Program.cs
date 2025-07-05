ab91092446042558e447a25484bfecc470d68c4e
juicedapples <ivana.r.vazquez.official@gmail.com>
2025-07-05 11:00:07 -0600

    Completed Core requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != magicNumber);
+        Console.WriteLine("You guessed it!");
     }
 }
