9f4b8a29592806dac70cf929700d51f159d521dc
juicedapples <ivana.r.vazquez.official@gmail.com>
2025-07-04 18:10:18 -0600

    updated asking favorite color

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,5 +6,15 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("This is in C#.");
+
+        int number = 5;
+        number = 8;
+        number = number + 3;
+
+        string color = "blue";
+
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
     }
 }
