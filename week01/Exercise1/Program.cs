2f908612134c5a093dd7ba09507164e924edf5b4
juicedapples <ivana.r.vazquez.official@gmail.com>
2025-07-04 18:25:38 -0600

    Updated program to meet w01 assigment requirements

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,17 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        Console.WriteLine("This is in C#.");
 
-        int number = 5;
-        number = 8;
-        number = number + 3;
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
 
-        string color = "blue";
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
 
-        Console.WriteLine("What is your favorite color?");
-        string color = Console.ReadLine();
-        Console.WriteLine(color);
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
