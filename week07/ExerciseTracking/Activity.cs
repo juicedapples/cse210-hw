51fbbb9bf4efc4591f868aa398ad3e9de66f789f
juicedapples <ivana.r.vazquez.official@gmail.com>
2026-04-15 21:46:36 -0600

    completed Activity and Cycling files

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,5 +1,4 @@
 using System;
-//base class
 public abstract class Activity
 {
     private string _date;
@@ -21,4 +20,20 @@ public abstract class Activity
         return _date;
     }
 
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min)- " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
 }
--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -3,4 +3,30 @@ using System;
 public class Cycling : Activity
 {
     private double _speed;
+
+    public Cycling(string date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        if (speed <= 0)
+        {
+            throw new ArgumentException("Speed must be greater than 0.");
+        }
+
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * GetMinutes()) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
 }
